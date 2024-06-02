83ce688b11c555ec8d1ce1014cfc2feb1a26a18d
priyasharanam1 <p1910413219@gmail.com>
2024-06-03 00:21:01 +0530

    add cart, auth

--- server.js
+++ server.js
@@ -10,14 +10,27 @@ const PORT = process.env.PORT || 3000
 //Assets
 
 app.use(express.static('public')) //serves static files from public folder
-app.get('/', (req,res)=>{
-     res.render("home")  //the file inside views folder
-})
 
 //set template engine
 app.use(expressLayouts)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
+app.get('/', (req,res)=>{
+    res.render("home")  //the file inside views folder
+})
+
+app.get('/cart', (req,res)=>{
+   res.render('customers/cart')
+})
+
+app.get('/login', (req,res)=>{
+    res.render('auth/login')
+ })
+
+ app.get('/register', (req,res)=>{
+    res.render('auth/register')
+ })
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
